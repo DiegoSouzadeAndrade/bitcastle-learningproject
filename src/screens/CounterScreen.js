2431eebae82fa746fc3c7ab69d88f69f17cd8061
DiegoSouzadeAndrade <diego.souzadeandrade@gmail.com>
2020-03-28 10:33:08 -0300

    Desktop commit, classes 55 to 59. Refactoring ColorScreen and Counter Screen using Reducer.

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,25 +1,41 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 
+const COUNTER_INCREMENT = 1;
+
+const reducer = (state, action) => {
+    //state === {counter: number}
+    //action === {type: 'increase' || 'decrease' , payload: 1}
+
+    switch(action.type){
+        case 'increase':
+            return {...state, counter: state.counter + action.payload};
+        case 'decrease':
+            return {...state, counter: state.counter - action.payload};
+        default:
+            return state;
+    }
+}
 
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, {counter: 0})
+    const {counter} = state;
 
     return (
         <View>
             <Button 
                 title="Increase" 
                 onPress={( )=>{
-                    setCounter(counter + 1);
+                    dispatch({type:'increase', payload: COUNTER_INCREMENT})
                 }}
             />
             <Button 
                 title="Decrease" 
                 onPress={()=>{
-                    setCounter(counter - 1);                
+                    dispatch({type:'decrease', payload: COUNTER_INCREMENT})                
                 }}
             />
-            <Text>Current Count: {counter}</Text>
+            <Text>Current Count: {state.counter}</Text>
         </View>
     );
 };
